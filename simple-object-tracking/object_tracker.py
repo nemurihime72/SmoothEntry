b6879c0ff170d71039fb37b0864a4f9c0f3015fb
nemurihime72 <s10195732@connect.np.edu.sg>
2021-01-31 16:41:49 +0800

    update tracker

--- simple-object-tracking/object_tracker.py
+++ simple-object-tracking/object_tracker.py
@@ -7,7 +7,8 @@
 from pyimagesearch.centroidtracker import CentroidTracker
 from imutils.video import VideoStream
 from imutils.video import FPS
-from imutils.object_detection import non_max_suppression	
+import nms
+#from imutils.object_detection import non_max_suppression	
 import numpy as np
 import argparse
 import imutils
@@ -16,9 +17,9 @@ import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
+ap.add_argument("-p", "--prototxt",
 	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
+ap.add_argument("-m", "--model",
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
@@ -31,23 +32,16 @@ ct = CentroidTracker()
 HOGCV = cv2.HOGDescriptor()
 HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
-
 # load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+#print("[INFO] loading model...")
+#net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 # initialize the video stream and allow the camera sensor to warmup
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
+p1List = []
 # loop over the frames from the video stream
 while True:
 	# read the next frame from the video stream and resize it
@@ -58,50 +52,17 @@ while True:
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
 		
-	bounding_box_coordinates, weights = HOGCV.detectMultiScale(frame, winStride = (8, 8), padding = (8, 8), scale = 1.03)
+	bounding_box_coordinates, weights = HOGCV.detectMultiScale(frame, winStride = (6, 6), padding = (16, 16), scale = 1.05)
 	rects = []
-	'''# construct a blob from the frame, pass it through the network,
-	# obtain our output predictions, and initialize the list of
-	# bounding box rectangles
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
-		(104.0, 177.0, 123.0))
-	net.setInput(blob)
-	detections = net.forward()
-	rects = []
-
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# filter out weak detections by ensuring the predicted
-		# probability is greater than a minimum threshold
-		if detections[0, 0, i, 2] > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object, then update the bounding box rectangles list
-			box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-			rects.append(box.astype("int"))
-
-			# draw a bounding box surrounding the object so we can
-			# visualize it
-			(startX, startY, endX, endY) = box.astype("int")
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				(0, 255, 0), 2)
-			#print("startx: ", startX, "starty:", startY)
-			#print("endx: ", endX, "endy:", endY)
-
-			# draw the prediction on the frame
-			#label = "{}: {:.2f}%".format(CLASSES[box],
-			#	confidence * 100)
-			#cv2.rectangle(frame, (startX, startY), (endX, endY),
-			#	COLORS[box], 2)
-			#y = startY - 15 if startY - 15 > 15 else startY + 15
-			#cv2.putText(frame, label, (startX, y),
-			#	cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[box], 2)'''
+	person = 0
 	for x,y,w,h in bounding_box_coordinates:
-        person += 1
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0) ,2)
-        cv2.putText(frame, '{0} {1}'.format(x, y), (x, y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0, 255), 1)
-        cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 1)
+		person += 1
+		cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0) ,2)
+		cv2.putText(frame, '{0} {1}'.format(x, y), (x, y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0, 255), 1)
+		cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 1)
 		rects.append([x, y, x+w, y+h])
 
+	
 	# update our centroid tracker using the computed set of bounding
 	# box rectangles
 	objects = ct.update(rects)
@@ -111,10 +72,12 @@ while True:
 		# draw both the ID of the object and the centroid of the
 		# object on the output frame
 		text = "ID {}".format(objectID)
-		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-		print(centroid[0], centroid[1])
+		
+		if objectID == 1:	
+			p1List.append([centroid[0], centroid[1]])
+
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
@@ -123,7 +86,9 @@ while True:
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-
+	elif key == ord("p"):	
+		for i in p1List:	
+			print(i)
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
